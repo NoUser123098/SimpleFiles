c8e8db8675e06226028b88314fdb2d7f960c76a5
NoUser123098 <31293075+NoUser123098@users.noreply.github.com>
2017-09-17 08:13:47 -0500

    Update simple.js

--- simple.js
+++ simple.js
@@ -4,12 +4,4 @@ SimpleJS
 By: DTS123098
 ---------
 */
-
-//Variables for short code
-var id = document.getElementbyId();
-var cssColor = id("textColor").style.color();
-var cssBgColor = id("bgColor").style.backgroundColor();
-var cssBorderType = id("borderStyle").style.borderStyle();
-var cssBorderTop = id("borderTop").style.borderTop();
-var cssBorderBottom = id("borderBottom").style.borderBottom();
-var cssBorderRight = id("borderRight").style.borderRight();
+var bgColorRed = document.getElementById("bgColorRed").style.backgroundColor = "red";
