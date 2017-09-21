4a261624253d6421eadd28c756f14abbef49844a
NoUser123098 <31293075+NoUser123098@users.noreply.github.com>
2017-09-21 16:10:04 -0500

    Update simple.js

--- simple.js
+++ simple.js
@@ -7,8 +7,12 @@ By: DTS123098
 
 //Styles
 $(function(){
-  var bgColorRed = $("bgRed").css("background-color", "red");
-  var bgColorCrimson = $("bgCrim").css("background-color", "crimson");
-  var bgColorBlue = $("bgBlue").css("background-color", "blue");
-  var bgColorDodgerBlue = $("bgDodBlue").css("background-color", "dodgerblue");
+  var bgColorRed = $("#bgRed").css("background-color", "red");
+  var bgColorCrimson = $("#bgCrim").css("background-color", "crimson");
+  var bgColorBlue = $("#bgBlue").css("background-color", "blue");
+  var bgColorDodgerBlue = $("#bgDodBlue").css("background-color", "dodgerblue");
+  var bgColorPink = $("#bgPink").css("background-color", "pink");
+  var bgColorDeepPink = $("#bgDeepPink").css("background-color", "deeppink");
+  var bgColorHotPink = $("#bgHotPink").css("background-color", "hotpink");
+  var bgColorMagenta = $("#bgMagenta").css("background-color", "magenta");
 });
