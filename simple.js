0e65c6e41ea610b4b0b783d6fc668432d8b4cfff
NoUser123098 <31293075+NoUser123098@users.noreply.github.com>
2017-11-15 15:38:22 -0600

    Update simple.js

--- simple.js
+++ simple.js
@@ -6,16 +6,5 @@ By: DTS123098
 */
 
 $(function(){
-  var bgColorRed = $("#bgRed").css("background-color", "red");
-  var bgColorCrimson = $("#bgCrim").css("background-color", "crimson");
-  var bgColorBlue = $("#bgBlue").css("background-color", "blue");
-  var bgColorDodgerBlue = $("#bgDodBlue").css("background-color", "dodgerblue");
-  var bgColorPink = $("#bgPink").css("background-color", "pink");
-  var bgColorDeepPink = $("#bgDeepPink").css("background-color", "deeppink");
-  var bgColorHotPink = $("#bgHotPink").css("background-color", "hotpink");
-  var bgColorMagenta = $("#bgMagenta").css("background-color", "magenta");
-  var bgColorPurple = $("#bgPurple").css("background-color", "purple");
-  var bgColorIndigo = $("#bgIndigo").css("background-color", "indigo");
-  var bgColorOrange = $("#bgOrange").css("background-color", "orange");
-  var bgColorRedOrange = $("#bgRedOrange").css("background-color", "orangered");
-  var bgColorDarkOrange = $("bgDarkOrange").css("background-color", "darkorange");
+  
+});
