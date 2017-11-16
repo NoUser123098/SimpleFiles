fa161dba48b55e110541f97da4e9939074ebed61
NoUser123098 <31293075+NoUser123098@users.noreply.github.com>
2017-11-16 11:26:06 -0600

    Update simple.js

--- simple.js
+++ simple.js
@@ -1,10 +1,23 @@
 /*
----------
+---------------
 SimpleJS
 By: DTS123098
----------
+---------------
 */
 
 $(function(){
   var red_aaaa = $("#red-1").css("background-color", "#ff4d4d");
+  var red_aaab = $("#red-2").css("background-color", "#ff3333");
+  var red_aaac = $("#red-3").css("background-color", "#ff1a1a");
+  var red = $("#red").css("background-color", "red");
+  var red_aaad = $("#red-4").css("background-color", "#e60000");
+  var red_aaae = $("#red-5").css("background-color", "#cc0000");
+  var red_aaaf = $("#red-6").css("background-color", "#b30000");
+  var blue_aaaa = $("#blue-1").css("background-color", "#4d4dff");
+  var blue_aaab = $("#blue-2").css("background-color", "#3333ff");
+  var blue_aaac = $("#blue-3").css("background-color", "#1a1aff");
+  var blue = $("#blue").css("background-color", "blue");
+  var blue_aaad = $("#blue-4").css("background-color", "#0000e6");
+  var blue_aaae = $("#blue-5").css("background-color", "#0000cc");
+  var blue_aaaf = $("#blue-6").css("background-color", "#0000b3");
 });
