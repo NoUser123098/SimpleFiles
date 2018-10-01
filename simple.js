3d517fe79f433e1b8d32ef0ee51a46a8c7a4bdc7
NoUser123098 <31293075+NoUser123098@users.noreply.github.com>
2018-10-01 08:07:50 -0500

    Update simple.js

--- simple.js
+++ simple.js
@@ -1,37 +1,10 @@
 /*
 ---------------
 SimpleJS
-By: DTS123098
+By: NoUser123098
 ---------------
 */
 
 $(function(){
-  var bg_red_aaaa = $("#bg-red-1").css("background-color", "#ff4d4d");
-  var bg_red_aaab = $("#bg-red-2").css("background-color", "#ff3333");
-  var bg_red_aaac = $("#bg-red-3").css("background-color", "#ff1a1a");
-  var bg_red = $("#bg-red").css("background-color", "red");
-  var bg_red_aaad = $("#bg-red-4").css("background-color", "#e60000");
-  var bg_red_aaae = $("#bg-red-5").css("background-color", "#cc0000");
-  var bg_red_aaaf = $("#bg-red-6").css("background-color", "#b30000");
-  var bg_blue_aaaa = $("#bg-blue-1").css("background-color", "#4d4dff");
-  var bg_blue_aaab = $("#bg-blue-2").css("background-color", "#3333ff");
-  var bg_blue_aaac = $("#bg-blue-3").css("background-color", "#1a1aff");
-  var bg_blue = $("#bg-blue").css("background-color", "blue");
-  var bg_blue_aaad = $("#bg-blue-4").css("background-color", "#0000e6");
-  var bg_blue_aaae = $("#bg-blue-5").css("background-color", "#0000cc");
-  var bg_blue_aaaf = $("#bg-blue-6").css("background-color", "#0000b3");
-  var bg_dodgerblue_aaaa = $("#bg-dodgerblue-1").css("background-color", "#66b3ff");
-  var bg_dodgerblue_aaab = $("#bg-dodgerblue-2").css("background-color", "#4da6ff");
-  var bg_dodgerblue_aaac = $("#bg-dodgerblue-3").css("background-color", "#3399ff");
-  var bg_dodgerblue = $("#bg-dodgerblue").css("background-color", "dodgerblue");
-  var bg_dodgerblue_aaad = $("#bg-dodgerblue-4").css("background-color", "#1a8cff");
-  var bg_dodgerblue_aaae = $("#bg-dodgerblue-5").css("background-color", "#0080ff");
-  var bg_dodgerblue_aaaf = $("#bg-dodgerblue-6").css("background-color", "#0073e6");
-  var bg_deeppink_aaaa = $("#bg-deeppink-1").css("background-color", "#ff4dac");
-  var bg_deeppink_aaab = $("#bg-deeppink-2").css("background-color", "#ff33a0");
-  var bg_deeppink_aaac = $("#bg-deeppink-3").css("background-color", "#ff1a94");
-  var bg_deeppink = $("#bg-deeppink").css("background-color", "deeppink");
-  var bg_deeppink_aaad = $("#bg-deeppink-4").css("background-color", "#ff0088");
-  var bg_deeppink_aaae = $("#bg-deeppink-5").css("background-color", "#e6007a");
-  var bg_deeppink_aaaf = $("#bg-deeppink-6").css("background-color", "#cc006d");
+  
 });
